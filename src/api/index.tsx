b5eec43563979e5cc3cc23773d0159cfa712a8f3
Steven LIAO <stevenliao19900326@gmail.com>
2020-02-24 14:52:55 +1100

    Hide the API key

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -6,7 +6,8 @@ import jsonp from 'jsonp';
 // the API call for getting the weather datat based on the coordinators and units
 export const reqWeather = (lat: number, long: number, units: string) => {
     return new Promise((resolve, reject) => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=5481d01e1f55fc521a219134e8545e8e&units=${units}`;
+        const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=${units}`;
 
         // execute jsonp request
         jsonp(url, {}, (error, data) => {
